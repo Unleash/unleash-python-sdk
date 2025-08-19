7930767b3f5aee5be5cee4d72144cd1fb7ac1887
Simon Hornby <liquidwicked64@gmail.com>
2025-08-19 14:58:49 +0200

    chore: text change to kick build server

--- UnleashClient/api/features.py
+++ UnleashClient/api/features.py
@@ -72,7 +72,7 @@ def get_feature_toggles(
         if resp.status_code not in [200, 304]:
             log_resp_info(resp)
             LOGGER.warning(
-                "Unleash Client feature fetch failed due to unexpected HTTP status code: %s",
+                "Unleash Client feature fetch failed due to an unexpected HTTP status code: %s",
                 resp.status_code,
             )
             raise Exception(
