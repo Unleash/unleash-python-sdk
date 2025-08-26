c656337ccdd1417f1c76dec5442fe8183ef9734d
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-08-26 17:27:47 +0200

    remove cleaned up files

--- UnleashClient/loader.py
+++
@@ -1,2 +0,0 @@
-# This file was deprecated and its functionality moved to BaseConnector.load_features().
-# The load_features function was removed as part of the connector refactor.
--- UnleashClient/periodic_tasks/fetch_and_load.py
+++
@@ -1,2 +0,0 @@
-# This file was deprecated and its functionality moved to connectors.
-# The functions were removed as part of the connector refactor.
