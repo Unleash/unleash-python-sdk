2cd5bf885d73cfb070c04ea6441013b152301f0f
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2025-08-28 09:23:20 +0200

    fix imports

--- UnleashClient/connectors/__init__.py
+++ UnleashClient/connectors/__init__.py
@@ -1,3 +1,5 @@
 from .base_connector import BaseConnector
 from .offline_connector import OfflineConnector
 from .polling_connector import PollingConnector
+
+__all__ = ["BaseConnector", "OfflineConnector", "PollingConnector"]
