757199be3243211bd89877eace37cacad714d986
irstavr <irstavr@gmail.com>
2026-07-20 19:37:59 +0200

    fix: remove sdk_flavor_fields() function + inline it in register+metrics
    the params are now Optional[str] instead of the helper's Any

--- UnleashClient/api/register.py
+++ UnleashClient/api/register.py
@@ -1,6 +1,7 @@
 import json
 from datetime import datetime, timezone
 from platform import python_implementation, python_version
+from typing import Optional
 
 import requests
 import yggdrasil_engine
@@ -13,7 +14,7 @@ from UnleashClient.constants import (
     SDK_NAME,
     SDK_VERSION,
 )
-from UnleashClient.utils import LOGGER, log_resp_info, sdk_flavor_fields
+from UnleashClient.utils import LOGGER, log_resp_info
 
 
 # pylint: disable=broad-except
@@ -27,8 +28,8 @@ def register_client(
     custom_options: dict,
     supported_strategies: dict,
     request_timeout: int,
-    sdk_flavor: str = None,
-    sdk_flavor_version: str = None,
+    sdk_flavor: Optional[str] = None,
+    sdk_flavor_version: Optional[str] = None,
 ) -> bool:
     """
     Attempts to register client with unleash server.
@@ -59,8 +60,12 @@ def register_client(
         "platformVersion": python_version(),
         "yggdrasilVersion": yggdrasil_engine.__yggdrasil_core_version__,
         "specVersion": CLIENT_SPEC_VERSION,
-        **sdk_flavor_fields(sdk_flavor, sdk_flavor_version),
     }
+    # Only sent when the client was configured with an integration flavor
+    if sdk_flavor:
+        registration_request["sdkFlavor"] = sdk_flavor
+    if sdk_flavor_version:
+        registration_request["sdkFlavorVersion"] = sdk_flavor_version
 
     try:
         LOGGER.info("Registering unleash client with unleash @ %s", url)
--- UnleashClient/periodic_tasks/send_metrics.py
+++ UnleashClient/periodic_tasks/send_metrics.py
@@ -1,11 +1,12 @@
 from platform import python_implementation, python_version
+from typing import Optional
 
 import yggdrasil_engine
 from yggdrasil_engine.engine import UnleashEngine
 
 from UnleashClient.api import send_metrics
 from UnleashClient.constants import CLIENT_SPEC_VERSION
-from UnleashClient.utils import LOGGER, sdk_flavor_fields
+from UnleashClient.utils import LOGGER
 
 
 def aggregate_and_send_metrics(
@@ -17,8 +18,8 @@ def aggregate_and_send_metrics(
     custom_options: dict,
     request_timeout: int,
     engine: UnleashEngine,
-    sdk_flavor: str = None,
-    sdk_flavor_version: str = None,
+    sdk_flavor: Optional[str] = None,
+    sdk_flavor_version: Optional[str] = None,
 ) -> None:
     metrics_bucket = engine.get_metrics()
 
@@ -37,8 +38,12 @@ def aggregate_and_send_metrics(
         "platformVersion": python_version(),
         "yggdrasilVersion": yggdrasil_engine.__yggdrasil_core_version__,
         "specVersion": CLIENT_SPEC_VERSION,
-        **sdk_flavor_fields(sdk_flavor, sdk_flavor_version),
     }
+    # Only sent when the client was configured with an integration flavor
+    if sdk_flavor:
+        metrics_request["sdkFlavor"] = sdk_flavor
+    if sdk_flavor_version:
+        metrics_request["sdkFlavorVersion"] = sdk_flavor_version
 
     if impact_metrics:
         metrics_request["impactMetrics"] = impact_metrics
